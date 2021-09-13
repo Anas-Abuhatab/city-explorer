670b5be3bcb85aff9ae9bece02cdb93c6eaa5d32
rymbaniali <rym.ayasrah@hotmail.com>
2021-09-13 23:10:16 +0300

    backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Location from './components/Location';
 import axios from 'axios';
 import Form from './components/Form';
-import ErrorCard from './components/ErrorCard';
+// import ErrorCard from './components/ErrorCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   constructor(props) {
@@ -13,17 +13,14 @@ class App extends Component {
       longitude: "",
       showData: false,
       showError: false,
-
+      weatherData:[],
+      status:""
     }
   }
   handleLocation = (e) => {
-
     let display_name = e.target.value;
-
-
     this.setState({
       display_name: display_name
-
     })
   }
   handleCloce = (e) => {
@@ -32,21 +29,13 @@ class App extends Component {
     })
     console.log('object')
   }
-
-  
-
   handleSubmit = (e) => {
-
     e.preventDefault();
-
-
     let config = {
       method: "GET",
       baseURL: `https://api.locationiq.com/v1/autocomplete.php?
       key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
     }
-   
-    
     axios(config).then(res => {
       let responseData = res.data[0]
       this.setState({
@@ -54,25 +43,28 @@ class App extends Component {
         longitude: responseData.lon,
         latitude: responseData.lat,
         showData: true,
-      });
-
-    }).catch(error => {
         
-        if (error.response) {
-        return this.setState({
-          showError: true
-          });
-      }
-    });
+      });
+    }).then(()=>{
+      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather-data?lat=${this.state.latitude}&lon=${this.state.longitude}`)
+      .then(res=>{
+        this.setState({
+          weatherData:  res.data,
+          status:""
+        })
+      });
+    }).catch(err=>{
+      this.setState({
+        status: "Please inter (Amman,Paris,Seattle) for weather info"
+      })
+      console.log(err)
+    })
   }
-
-
-
-
-
   render() {
     return (
+
       <>
+     
         <Form handleSubmit={this.handleSubmit}
           handleLocation={this.handleLocation} />
         {this.state.showData &&
@@ -80,17 +72,26 @@ class App extends Component {
             latitude={this.state.latitude}
             longitude={this.state.longitude} />
         }
-
+         {
+          this.state.weatherData.map(item=>{
+            return<>
+            <h2>{item.date}</h2>
+            <h2>{item.description}</h2>
+              
+            </>
+          })
+          
+        }
+        <h2>{this.state.status}</h2>
         <img src={`https://maps.locationiq.com/v3/staticmap?
         key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}
         &center=${this.state.latitude},${this.state.longitude}&zoom=1-18`} alt="jhhy" />
-
-
-        <ErrorCard
+        {/* <ErrorCard
           showError={this.state.showError}
           handleCloce={this.handleCloce}
-        />
-      </>
+        /> */}
+        
+          </>
     )
   }
 }
--- src/components/ErrorCard.js
+++ src/components/ErrorCard.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import {
-    Modal,
-    Button
-} from 'react-bootstrap'
+// import React, { Component } from 'react';
+// import {
+//     Modal,
+//     Button
+// } from 'react-bootstrap'
 
 
-class ErrorCard extends Component {
-    render() {
-        return (
+// class ErrorCard extends Component {
+//     render() {
+//         return (
 
-            <>
+//             <>
 
 
-                <Modal show={this.props.showError} onHide={this.props.handleCloce}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Error Massage</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body><h2>"error": "Unable to geocode"</h2></Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.props.handleCloce}>
-                            Close
-                        </Button>
+//                 <Modal show={this.props.showError} onHide={this.props.handleCloce}>
+//                     <Modal.Header closeButton>
+//                         <Modal.Title>Error Massage</Modal.Title>
+//                     </Modal.Header>
+//                     <Modal.Body><h2>"error": "Unable to geocode"</h2></Modal.Body>
+//                     <Modal.Footer>
+//                         <Button variant="secondary" onClick={this.props.handleCloce}>
+//                             Close
+//                         </Button>
 
-                    </Modal.Footer>
-                </Modal>
+//                     </Modal.Footer>
+//                 </Modal>
 
                 
-            </>
-        )
-    }
+//             </>
+//         )
+//     }
 
-}
-export default ErrorCard
+// }
+// export default ErrorCard
